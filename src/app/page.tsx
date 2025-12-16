4a43e29b88be3fb0b4d691eaaad7284d3c91301d
SheHab970 <shehab202205097@gmail.com>
2025-12-16 14:35:17 +0200

    Add hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import Image from "next/image";
+
 const HomePage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+      <div className="relative aspect-3/1 mb-12"> 
+        <Image src="/featured.png" alt="Featured Product" fill/>
+      </div>
+    </div>
   );
 }
 
