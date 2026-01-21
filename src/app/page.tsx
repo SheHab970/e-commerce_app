d3b026ad5c6f0f637a1823fe0dc84c847a9ec095
SheHab970 <shehab202205097@gmail.com>
2026-01-21 18:10:13 +0200

    edite margin for media

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const HomePage = () => {
   return (
     <div>
-      <div className="relative aspect-3/1 mb-12"> 
+      <div className="relative aspect-3/1 mb-4 sm:mb-6 md:mb-8 lg:mb-10 xl:mb-12"> 
         <Image src="/featured.png" alt="Featured Product" fill/>
       </div>
 
